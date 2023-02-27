17676ace71a775e2fae8ccd8a1a48f9fff8c48cb
Leonid <ztvlnd777@gmail.com>
2023-02-27 14:18:21 +0200

    100

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,14 +6,13 @@ console.log(galleryItems);
 const items = galleryItems.map(({original, preview, description})=> {
   return `<div class="gallery__item">
   <a class="gallery__link" href="${original}" >
-  <img
-    class="gallery__image"
-    src="${preview}"
-    data-source="${original}"
-    alt="${description}"
-  />
-</a>
-</div>`}).join("");
+   <img
+     class="gallery__image"
+     src="${preview}"
+     data-source="${original}"
+     alt="${description}"/>
+  </a>
+       </div>`}).join("");
     
 const galleryBox = document.querySelector('.gallery');
 
@@ -22,18 +21,31 @@ galleryBox.innerHTML = items;
 galleryBox.addEventListener('click', onLinkClick);
 
 function onLinkClick(event) {
-    event.preventDefault();
-    if (!event.target.classList.contains('gallery__image')) { return; }
+  event.preventDefault();
+  
+  if (!event.target.classList.contains('gallery__image')) { return };
+ 
+  const instance = basicLightbox.create(`<img width="1400" height="900" src="${event.target.dataset.source}"/>`)
 
-    
+  window.addEventListener('keydown', onEscKeyPress);
 
+  instance.show();
 
-
-
-
-
-	
-	
-
-
+  function onEscKeyPress(event) {
+    if (event.code === 'Escape') { instance.close() }
+  }
+  function stopListener() {
+    if (!instance.visible()) {
+      window.removeEventListener('keydown', onEscKeyPress);
+    }
+  }
+   console.log(event.currentTarget)
 }
+
+
+
+
+
+
+
+
