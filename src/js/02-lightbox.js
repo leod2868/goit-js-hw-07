0374cedbda1ba845187f3005fdd3e1b68f49f91d
Leonid <ztvlnd777@gmail.com>
2023-02-27 20:10:29 +0200

    finish

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const items = galleryItems.map(({original, preview, description})=> {
+  return `<a class="gallery__item" href="${original}" >
+   <img
+     class="gallery__image"
+     src="${preview}"
+     alt="${description}"/>
+  </a>`}).join("");
+    
+const galleryBox = document.querySelector('.gallery');
+
+galleryBox.innerHTML = items;
+
+function stopLoading(event) {
+    event.preventDefault();
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
